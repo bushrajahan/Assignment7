6908ab904e7d76d9f0dc0d14d9aa2db6f2130426
bushrajahan <132329761+bushrajahan@users.noreply.github.com>
2023-09-16 20:33:56 +0600

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ function App() {
     
       <h1 className='text-center text-bold'>Course Registation</h1>
        <div className='flex flex-col lg:flex-row gap-2'>
-         <div className='md:w-9/12'>
+         <div className='lg:w-9/12'>
             <Cards items ={items} handleCard={handleCard}></Cards>
          </div>
          <div className=''>
